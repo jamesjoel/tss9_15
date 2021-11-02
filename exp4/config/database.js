683af3cc39d9e58c01d95fcf5ca83eafd183f4c6
Converge Info services <admin@cnvg.in>
2021-11-02 09:56:14 +0530

    sapmle

--- exp4/config/database.js
+++ exp4/config/database.js
@@ -1,8 +1,6 @@
 var mongodb = require("mongodb");
-var mongodb = require("mongodb");
-var mongodb = require("mongodb");
 var MongoClient = mongodb.MongoClient;
-var MongoClient = mongodb.MongoClient;
+
 /* test */ var a = "rohit";
 module.exports = (cb)=>{
     MongoClient.connect("mongodb://localhost:27017", cb);
